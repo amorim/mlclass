6c3d220b869654431f25b032dc2fe2c2a012879b
NelsonGomesNeto <ngn@ic.ufal.br>
2018-02-08 11:12:00 -0300

    All average methods were pretty bad, trying to remove all invalid rows

--- 01_Preprocessing/diabetes_csv.py
+++ 01_Preprocessing/diabetes_csv.py
@@ -14,15 +14,13 @@ from sklearn.neighbors import KNeighborsClassifier
 import requests
 
 print('\n - Lendo o arquivo com o dataset sobre diabetes')
-data = pd.read_csv('diabetes_fixed_dataset_NoPedi_NoAge.csv')
+data = pd.read_csv('diabetes_medianNormByMinMax_dataset.csv')
 
 # Criando X and y par ao algorítmo de aprendizagem de máquina.\
 print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
 # Caso queira modificar as colunas consideradas basta algera o array a seguir.
 feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
-                'Insulin', 'BMI']
-#feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
-#                'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+                'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 X = data[feature_cols]
 y = data.Outcome
 
@@ -33,7 +31,7 @@ neigh.fit(X, y)
 
 #realizando previsões com o arquivo de
 print(' - Aplicando modelo e enviando para o servidor')
-data_app = pd.read_csv('diabetes_app_NoPedi_NoAge.csv')
+data_app = pd.read_csv('diabetes_app.csv')
 y_pred = neigh.predict(data_app)
 
 # Enviando previsões realizadas com o modelo para o servidor
--- 01_Preprocessing/fill.py
+++ 01_Preprocessing/fill.py
@@ -1,24 +1,51 @@
 import pandas as pd
 from math import isnan
 
-data = pd.read_csv('diabetes_dataset.csv')
+def getMedian(array):
+    if (len(array) % 2):
+        return(array[len(array) // 2])
+    else:
+        return((array[len(array) // 2] + array[len(array) // 2 - 1]) / 2)
+
+def playWithAverages(data, cols):
+    average, median, maximum, minimum = {}, {}, {}, {}
+    for i in cols:
+        a, validValues, values = 0, [], 0
+        for j in data[i]:
+            if (not isnan(j)):
+                a += j
+                values += 1
+                validValues += [j]
+        median[i] = getMedian(validValues)
+        a /= values
+        average[i] = a
+        maximum[i] = max(validValues)
+        minimum[i] = min(validValues)
+    print(maximum)
+
+    for i in cols:
+        for j in range(len(data[i])):
+            if (isnan(data[i][j])):
+                data.set_value(j, i, median[i])
+                #data[i][j] = median[i]
+            data.set_value(j, i, (data[i][j] - minimum[i]) / (maximum[i] - minimum[i]))
+
+def removeRows(data, cols):
+    removed = {}
+    for i in cols:
+        for j in range(len(data[i]) - 1, 0, -1):
+            if (j in removed): continue
+            if (isnan(data[i][j])):
+                print("dropping:", j)
+                data = data.drop([j])
+                removed[j] = 1
+                #removeRows(data, cols)
+    return(data)
+
+data = pd.read_csv('diabetes_dataset.csv').astype('float')
 cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
                 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
-res = {}
-for i in cols:
-    mean, values = 0, 0
-    for j in data[i]:
-        if (not isnan(j)):
-            mean += j
-            values += 1
-    mean /= values
-    res[i] = mean
-print(res)
 
-for i in cols:
-    for j in range(len(data[i])):
-        if (isnan(data[i][j])):
-            data[i][j] = res[i]
+data = removeRows(data, cols)
 print(data)
-data.to_csv("diabetes_fixed_dataset.csv")
-
+data.to_csv("diabetes_removeAllInvalidRows_dataset.csv")
